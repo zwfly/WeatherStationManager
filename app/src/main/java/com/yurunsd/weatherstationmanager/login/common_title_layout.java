819b8fda3dc503cc12ad1bd85b61462a3c26b2d0
zwfly <604693290@qq.com>
2017-06-19 08:23:29 +0800

    Initial commit

---
+++ app/src/main/java/com/yurunsd/weatherstationmanager/login/common_title_layout.java
@@ -0,0 +1,12 @@
+package com.yurunsd.weatherstationmanager.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class common_title_layout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/yurunsd/weatherstationmanager/utils/JSONUtils.java
@@ -0,0 +1,8 @@
+package com.yurunsd.weatherstationmanager.utils;
+
+/**
+ * Created by admin on 2017/6/17.
+ */
+
+public class JSONUtils {
+}
